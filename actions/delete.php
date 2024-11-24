fa38aa891c423b1cce9a7d46e7ef169acec2987a
igorkita <igorirelia14@gmail.com>
2024-11-24 20:56:21 -0300

    fix: Ajuste erro ao deletar a transação

--- actions/delete.php
+++ actions/delete.php
@@ -2,12 +2,14 @@
 session_start();
 require_once('connect.php');
 
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+if (isset($_POST['transaction_id']) && isset($_POST['month_id'])) {
+    $transaction_id = $_POST['transaction_id'];
     
-    $sql = "DELETE FROM transactions WHERE id = $id";
+    $sql = "DELETE FROM transactions WHERE id = $transaction_id";
     mysqli_query($conn, $sql);
-    header('Location: ../historic.php?id=' . $id);
+
+    $month_id = $_POST['month_id'];
+    header('Location: ../historic.php?id=' . $month_id);
     exit();
 }
 ?>
--- historic.php
+++ historic.php
@@ -170,11 +170,14 @@ if (isset($_GET['id'])) {
                                             </svg>
                                             Editar
                                         </span>
-                                        <a href="actions/delete.php?id=<?= $transaction['id']; ?>" class="flex items-center justify-center cursor-pointer bg-rose-600 rounded size-8">
+                                        <form action="actions/delete.php" method="POST" class="flex items-center justify-center cursor-pointer bg-rose-600 rounded size-8">
+                                            
+                                            <input type="hidden" name="transaction_id" value="<?= $transaction['id']; ?>">
                                             <input type="hidden" name="month_id" value="<?= intval($_GET['id']); ?>">
-
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-zinc-50"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
-                                        </a>
+                                            <button type="submit">    
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-zinc-50"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
+                                            </button>
+                                        </form>
                                     </th>
                                 </tr>
                             <?php endforeach; ?>
